7d87b758cdf5cf6a169a111ab2b86a41dce9d271
xumengxiang <xumengxiang@corp.netease.com>
2026-03-05 19:31:10 +0800

    feat: 云信 NIM SDK 改用 NPM 包、添加消息已读回执、修复只处理线上消息

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
             outDir: 'dist-electron',
             minify: false,
             rollupOptions: {
-              external: ['sql.js', 'discord.js', 'zlib-sync', '@discordjs/opus', 'bufferutil', 'utf-8-validate', 'node-nim'],
+              external: ['sql.js', 'discord.js', 'zlib-sync', '@discordjs/opus', 'bufferutil', 'utf-8-validate', 'node-nim', 'nim-web-sdk-ng'],
               output: {
                 // Keep CJS format (default), but load via ESM loader.mjs
                 inlineDynamicImports: true,
